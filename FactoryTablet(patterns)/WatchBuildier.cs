956941b9cc67589c172adb81e03dc043d4f18d55
sergey <goblinsan@mail.ru>
2023-01-27 15:25:01 +0300

    New device- watch

---
+++ FactoryTablet(patterns)/WatchBuildier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryTablet_patterns_
+{
+    public class WatchBuildier:DeviceBuildier
+    {
+
+
+
+        public WatchBuildier(IDevice device) : base(device)
+        {
+
+
+        }
+
+        public override void SetBody(string str)
+        {
+            Device.Body = $"{str} Watch Boby";
+        }
+
+        public override void SetModel(string str)
+        {
+            Device.Model = $"{str} Watch Model";
+        }
+
+        public override void SetName(string str)
+        {
+            Device.Name = $"{str} Watch Name";
+        }
+
+        public override void SetScreen(string str)
+        {
+            Device.Screen = $"{str} Watch Screen";
+        }
+
+
+    }
+}
